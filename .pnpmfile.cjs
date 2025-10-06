d767599aa13f5c7d426cf4848c0e211309cda9f8
travislucero <tlucero@aussiepetmobile.com>
2025-10-06 11:22:43 -0400

    Update .pnpmfile.cjs
    Fix: Standardize @lezer/common version in .pnpmfile.cjs and update lockfile

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   hooks: {
     readPackage(pkg) {
+      // Force ts-essentials to version 10.0.2
       if (pkg.dependencies && pkg.dependencies['ts-essentials']) {
         pkg.dependencies['ts-essentials'] = '10.0.2';
       }
@@ -11,6 +12,15 @@ module.exports = {
       if (pkg.optionalDependencies && pkg.optionalDependencies['ts-essentials']) {
         pkg.optionalDependencies['ts-essentials'] = '10.0.2';
       }
+
+      // Force @lezer/common to version 1.2.1
+      if (pkg.dependencies && pkg.dependencies['@lezer/common']) {
+        pkg.dependencies['@lezer/common'] = '1.2.1';
+      }
+      if (pkg.devDependencies && pkg.devDependencies['@lezer/common']) {
+        pkg.devDependencies['@lezer/common'] = '1.2.1';
+      }
+
       return pkg;
     },
   },
